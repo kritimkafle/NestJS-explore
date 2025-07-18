67d15055e2864aff6afa53e8a085b40c5d6d39a2
kritimkafle <backendthegtspl@gmail.com>
2025-07-18 17:09:54 +0545

    feat: implement custom logging service and integrate it into the application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/error.handler';
+// import { MyLoggerService } from './my-logger/my-logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * to apply Logger globally
+   * 
+   * const app = await NestFactory.create(AppModule, {
+   *   bufferLogs: true,
+   * });
+   * 
+   * app.useLogger(app.get(MyLoggerService));
+   */
+
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalFilters(new AllExceptionsFilter());
